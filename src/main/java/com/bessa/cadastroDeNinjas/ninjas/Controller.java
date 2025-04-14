da87175603be2a09f9929c6a56e84dcdda331800
Victor Bessa Marcelino <vitorbessilva@gmail.com>
2025-04-14 20:42:25 -0300

    feat: inclui aplicação de dto no service e no controller

--- src/main/java/com/bessa/cadastroDeNinjas/ninjas/Controller.java
+++ src/main/java/com/bessa/cadastroDeNinjas/ninjas/Controller.java
@@ -16,7 +16,7 @@ public class Controller {
 
 
     @PostMapping("/criar")
-    public NinjaModel criarNinja( @RequestBody NinjaModel ninja) {
+    public NinjaDTO criarNinja( @RequestBody NinjaDTO ninja) {
         return  ninjaService.criarNinja(ninja);
     }
 
--- src/main/java/com/bessa/cadastroDeNinjas/ninjas/NinjaService.java
+++ src/main/java/com/bessa/cadastroDeNinjas/ninjas/NinjaService.java
@@ -8,10 +8,14 @@ import java.util.Optional;
 @Service
 public class NinjaService {
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository){
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
+
+
 
     public List<NinjaModel>  listarTodos(){
         return ninjaRepository.findAll();
@@ -22,8 +26,10 @@ public class NinjaService {
         return  ninjaPorId.orElse(null);
     }
 
-    public  NinjaModel criarNinja(NinjaModel ninja){
-        return ninjaRepository.save(ninja);
+    public  NinjaDTO criarNinja(NinjaDTO ninjaDTO){
+       NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+       ninja = ninjaRepository.save(ninja);
+       return ninjaMapper.map(ninja);
     }
 
     public void deletarPorId(Long id){
